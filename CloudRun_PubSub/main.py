c41e1e31a307117c77516a738da152dbc44c3004
leonidasklv <97111773+leonidasklv@users.noreply.github.com>
2022-01-04 18:08:49 +0200

    Update main.py

--- CloudRun_PubSub/main.py
+++ CloudRun_PubSub/main.py
@@ -16,7 +16,22 @@
 import base64
 import os
 
+from concurrent import futures
+from google.cloud import pubsub_v1
 from flask import Flask, request
+
+
+def get_callback(
+    publish_future: pubsub_v1.publisher.futures.Future, data: str
+) -> Callable[[pubsub_v1.publisher.futures.Future], None]:
+    def callback(publish_future: pubsub_v1.publisher.futures.Future) -> None:
+        try:
+            # Wait 60 seconds for the publish call to succeed.
+            print(publish_future.result(timeout=60))
+        except futures.TimeoutError:
+            print(f"Publishing {data} timed out.")
+
+    return callback
 
 
 app = Flask(__name__)
@@ -30,22 +45,52 @@ app = Flask(__name__)
 def index():
     envelope = request.get_json()
     if not envelope:
-        msg = "no Pub/Sub message received"
+        msg = "no message received"
         print(f"error: {msg}")
         return f"Bad Request: {msg}", 400
 
     if not isinstance(envelope, dict) or "message" not in envelope:
-        msg = "invalid Pub/Sub message format"
+        msg = "invalid message format"
         print(f"error: {msg}")
         return f"Bad Request: {msg}", 400
 
     pubsub_message = envelope["message"]
     #print("Message Noumber: " , envelope)
+
     name = "World"
+
     if isinstance(pubsub_message, dict): ##and 'data' in pubsub_message:
         name = base64.b64decode(pubsub_message['data']).decode("utf-8").strip()
 
     print("Message Noumber: " , name)
+
+
+"""Pushing the message to a Pub/Sub topic with an error handler."""
+# In the above lines we are receiving a json file and now we want to push it to a pub/sub topic.
+# Bellow we push the received json file ( and more precise the data of the received message) to an already created topic. 
+# We can create multiple topics and have the destination that we want to sent them on the json file.
+# That maybe helpfull to seperate the receiving messages. Moreover later it will be efficient on the dataflow to choose one topic of received messages to 
+# store them in the right querry or to stream eventualy some of them and not all of them. 
+# 
+
+# This change of each diferent topic or project
+    project_id = "majestic-device-333311"
+    topic_id = "data-receiver"
+    
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+    publish_futures = []
+
+    # When you publish a message, the client returns a future.
+    publish_future = publisher.publish(topic_path, data.encode("utf-8"))
+    # Non-blocking. Publish failures are handled in the callback function.
+    publish_future.add_done_callback(get_callback(publish_future, data))
+    publish_futures.append(publish_future)
+
+    # Wait for all the publish futures to resolve before exiting.
+    futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+
+    print(f"Published messages with error handler to {topic_path}.")
 
     return ("", 204)
 
